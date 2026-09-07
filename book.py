4a717ded2061d9600f029e25937a4bf64b797a14
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-09-07 23:03:10 +0000

    booksEndPoint:created a dictionary list of books and called all the books in it

--- book.py
+++ book.py
@@ -2,10 +2,19 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+BOOKS = [
+    {'name': 'Abdul-Mumin', 'author': 'Author One', 'category': 'Computing'},
+    {'name': 'Abdul-Mumin', 'author': 'Author One', 'category': 'Computing'},
+    {'name': 'Rufai-Bawa', 'author': 'Author One', 'category': 'Economics'},
+    {'name': 'Rukayatu-Yakubu', 'author': 'Author One', 'category': 'Geography'},
+    {'name': 'Bintu-Bawa', 'author': 'Author One', 'category': 'Calculus'},
+    {'name': 'Abdul-Mumin', 'author': 'Author One', 'category': 'Economics'},
+    {'name': 'Tijani-Yakubu', 'author': 'Author One', 'category': 'Maths'},
+    {'name': 'Fatawu-Yakubu', 'author': 'Author One', 'category': 'English'},
 
-@app.get('/name')
+]
+
+
+@app.get('/books')
 async def my_first_end_point():
-    return {'name': 'Abdul-Mumin',
-            'father': 'Rufai Bawa',
-            'mother': 'Rukayatu Yakubu'
-            }
+    return BOOKS
