7bc4468367ce4c558e1bb375d974abef3a31dc48
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-09-09 17:32:18 +0000

    pathParameter&DynamicParameter:learnt path parameter and dynamic parameter

--- book.py
+++ book.py
@@ -5,16 +5,30 @@ app = FastAPI()
 BOOKS = [
     {'name': 'Abdul-Mumin', 'author': 'Author One', 'category': 'Computing'},
     {'name': 'Abdul-Mumin', 'author': 'Author One', 'category': 'Computing'},
-    {'name': 'Rufai-Bawa', 'author': 'Author One', 'category': 'Economics'},
-    {'name': 'Rukayatu-Yakubu', 'author': 'Author One', 'category': 'Geography'},
-    {'name': 'Bintu-Bawa', 'author': 'Author One', 'category': 'Calculus'},
-    {'name': 'Abdul-Mumin', 'author': 'Author One', 'category': 'Economics'},
-    {'name': 'Tijani-Yakubu', 'author': 'Author One', 'category': 'Maths'},
-    {'name': 'Fatawu-Yakubu', 'author': 'Author One', 'category': 'English'},
+    {'name': 'Rufai-Bawa', 'author': 'Author Three', 'category': 'Economics'},
+    {'name': 'Rukayatu-Yakubu', 'author': 'Author Four', 'category': 'Geography'},
+    {'name': 'Bintu-Bawa', 'author': 'Author Five', 'category': 'Calculus'},
+    {'name': 'Abdul-Mumin', 'author': 'Author Six', 'category': 'Economics'},
+    {'name': 'Tijani-Yakubu', 'author': 'Author Seven', 'category': 'Maths'},
+    {'name': 'Fatawu-Yakubu', 'author': 'Author Eight', 'category': 'English'},
 
 ]
 
 
 @app.get('/books')
-async def my_first_end_point():
+async def books():
     return BOOKS
+
+
+@app.get('/books/mybook')
+async def books():
+    return {'book': 'this is my favourite book'}
+
+
+# dynamic params must come after the static params
+@app.get('/books/{name}')
+async def books(name: str):
+    for book in BOOKS:
+        if book.get('name').casefold() == name.casefold():  # change to lower case letters because the url is lower
+            # case
+            return book
