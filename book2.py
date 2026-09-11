1f298899cd1c3bfcd9c38663812084bd6de20271
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-09-11 19:40:58 +0000

    pydantic: used pydantic to validate data input

--- book2.py
+++ book2.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,6 +19,15 @@ class Book:
         self.rating = rating
 
 
+# pydantic is a data validation libray use to validate data during development
+class CreateBook(BaseModel):  # create book type
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
 BOOKS = [
     Book(1, 'computer science', 'Abdul-Mumin', 'I love programming', 5),
     Book(2, 'calculus', 'Abdul-Mumin', 'calculus is good', 5),
@@ -31,3 +41,9 @@ BOOKS = [
 @app.get('/books')
 def get_all_books():
     return BOOKS
+
+
+@app.post('/books/create_book')
+async def add_book(new_book: CreateBook):
+    book = Book(**new_book.dict())  # the ** is helping to create property value pairs in the dictionary
+    BOOKS.append(book)
